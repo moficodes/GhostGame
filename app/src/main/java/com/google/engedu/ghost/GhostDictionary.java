7f9d627eefd7ae768798de734c683486d84e22ee
Mofizur Rahman <moficodes@gmail.com>
2019-05-11 23:42:50 -0400

    empty commit

--- app/src/main/java/com/google/engedu/ghost/GhostDictionary.java
+++ app/src/main/java/com/google/engedu/ghost/GhostDictionary.java
@@ -3,6 +3,7 @@ package com.google.engedu.ghost;
 /**
  * Created by apicard on 9/16/15.
  */
+
 public interface GhostDictionary {
     public final static int MIN_WORD_LENGTH = 4;
     boolean isWord(String word);
